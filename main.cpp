41c7d42657503de18385e9d29d20725a13674211
MikiKru <michal_kruczkowski@o2.pl>
2019-10-19 13:09:51 +0200

    cpp v5

--- main.cpp
+++ main.cpp
@@ -49,19 +49,32 @@ void exerciseC1(){
     cout << "Input: name, lastname, date of birth (YYYY-MM-DD), possition, salary_net" << endl;
     cin >> name >> lastname >> birthdate >> possition >> salary_net;
     // instrukcja warunkowa
-    if(cin.fail()){
-        cout << "Upsss" << endl;
-    } else {
+    if(!cin.fail()){
         cout << "Name: " << name << endl;
         cout << "Lastname: " << lastname << endl;
         cout << "Date of birth: " << birthdate << endl;
         cout << "Possition: " << possition << endl;
         cout << "Salary (net / gross): " << salary_net << " PLN" << " / " << salary_net * 1.23 << " PLN" << endl;
+    } else {
+        cout << "Upsss" << endl;
+    }
+}
+void exerciseC2(){
+    int a, b, c;
+    cout << "Input three numbers: " << endl;
+    cin >> a >> b >> c;
+    if(cin.fail()){
+        cout << "Invalid numbers " << endl;
+    } else{
+        // konwersja rozszerzająca
+        double avg = ((double)a + b + c)/3;
+        // konwersja zawężająca
+        cout << "AVG: " << ((int)avg) << endl;
     }
 }
 
 int main() {
     // wywołanie zawartości metody
-    exerciseC1();
+    exerciseC2();
     return 0;
 }
